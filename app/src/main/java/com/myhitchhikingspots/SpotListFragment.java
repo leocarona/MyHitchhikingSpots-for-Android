c67514cb51147794421b09b7673784e32d014f83
Leo Carona <leoncioleone@gmail.com>
2017-06-13 09:39:36 -0300

    Fetch address for all non gpsResolved spots - This is not fully implemented neither is it used at all yet

--- app/src/main/java/com/myhitchhikingspots/SpotListFragment.java
+++ app/src/main/java/com/myhitchhikingspots/SpotListFragment.java
@@ -2,7 +2,9 @@ package com.myhitchhikingspots;
 
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.location.Address;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,8 +13,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.crashlytics.android.Crashlytics;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.myhitchhikingspots.model.DaoMaster;
 import com.myhitchhikingspots.model.DaoSession;
 import com.myhitchhikingspots.model.Spot;
@@ -24,9 +28,31 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 
+import static com.mapbox.mapboxsdk.Mapbox.getApplicationContext;
+
 public class SpotListFragment extends Fragment {
     private RecyclerView recyclerView;
     List<Spot> spotList = new ArrayList<>();
+
+    /**
+     * Tracks whether the user has requested an address. Becomes true when the user requests an
+     * address and false when the address (or an error message) is delivered.
+     * The user requests an address by pressing the Fetch Address button. This may happen
+     * before GoogleApiClient connects. This activity uses this boolean to keep track of the
+     * user's intent. If the value is true, the activity tries to fetch the address as soon as
+     * GoogleApiClient connects.
+     */
+    protected boolean mAddressRequested;
+
+    /**
+     * The formatted location address.
+     */
+    protected Address mAddressOutput;
+
+    /**
+     * Receiver registered with this activity to get the response from FetchAddressIntentService.
+     */
+    private SpotFormActivity.AddressResultReceiver mResultReceiver;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,7 +68,7 @@ public class SpotListFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        Crashlytics.log(Log.INFO,TAG, "onResume was called");
+        Crashlytics.log(Log.INFO, TAG, "onResume was called");
         updateUI();
     }
 
@@ -69,4 +95,96 @@ public class SpotListFragment extends Fragment {
         if (this.isResumed())
             updateUI();
     }
+
+    /**
+     * Runs when user clicks the Fetch Address button. Starts the service to fetch the address if
+     * GoogleApiClient is connected.
+     *
+    public void fetchMissingAddressButtonHandler(View view) {
+
+        Spot lastAddedSpot = ((MyHitchhikingSpotsApplication) getApplicationContext()).getLastAddedSpot();
+        if (lastAddedSpot != null) {
+            LatLng pinPosition = new LatLng(lastAddedSpot.getLatitude(), lastAddedSpot.getLongitude());
+
+            // We only start the service to fetch the address if GoogleApiClient is connected.
+            if (pinPosition == null)
+                return;
+
+            fetchAddress(new MyLocation(pinPosition.getLatitude(), pinPosition.getLongitude()));
+        }else
+            Toast.makeText(getContext(), "No address to be fetched.", Toast.LENGTH_LONG).show();
+    }
+
+    public void fetchAddress(MyLocation loc) {
+        startIntentService(loc);
+
+        // If GoogleApiClient isn't connected, we process the user's request by setting
+        // mAddressRequested to true. Later, when GoogleApiClient connects, we launch the service to
+        // fetch the address. As far as the user is concerned, pressing the Fetch Address button
+        // immediately kicks off the process of getting the address.
+        mAddressRequested = true;
+        updateUIWidgets();
+    }
+
+    Intent fetchAddressServiceIntent;
+
+    /**
+     * Creates an intent, adds location data to it as an extra, and starts the intent service for
+     * fetching an address.
+     *
+    protected void startIntentService(MyLocation location) {
+        // Create an intent for passing to the intent service responsible for fetching the address.
+        fetchAddressServiceIntent = new Intent(this, FetchAddressIntentService.class);
+
+        // Pass the result receiver as an extra to the service.
+        fetchAddressServiceIntent.putExtra(Constants.RECEIVER, mResultReceiver);
+
+        // Pass the location data as an extra to the service.
+        fetchAddressServiceIntent.putExtra(Constants.LOCATION_DATA_EXTRA, location);
+
+        // Start the service. If the service isn't already running, it is instantiated and started
+        // (creating a process for it if needed); if it is running then it remains running. The
+        // service kills itself automatically once all intents are processed.
+        startService(fetchAddressServiceIntent);
+    }
+
+    protected void stopIntentService() {
+        if (!mAddressRequested || fetchAddressServiceIntent == null)
+            return;
+
+        stopService(fetchAddressServiceIntent);
+        mAddressRequested = false;
+        //updateUIWidgets();
+    }
+
+
+    protected void displayAddressOutput() {
+        if (mAddressOutput != null) {
+            mCurrentSpot.setCity(mAddressOutput.getLocality());
+            mCurrentSpot.setState(mAddressOutput.getAdminArea());
+            mCurrentSpot.setCountry(mAddressOutput.getCountryName());
+            mCurrentSpot.setCountryCode(mAddressOutput.getCountryCode());
+            mCurrentSpot.setGpsResolved(true);
+        } else {
+            mCurrentSpot.setCity("");
+            mCurrentSpot.setState("");
+            mCurrentSpot.setCountry("");
+            mCurrentSpot.setCountryCode("");
+            mCurrentSpot.setGpsResolved(false);
+        }
+
+        mLocationAddressTextView.setText(getString(mCurrentSpot));
+
+    }
+
+    @NonNull
+    private String getString(Spot mCurrentSpot) {
+        String spotLoc = "";
+        try {
+            spotLoc = spotLocationToString(mCurrentSpot).trim();
+        } catch (Exception ex) {
+            Crashlytics.logException(ex);
+        }
+        return spotLoc;
+    }*/
 }
