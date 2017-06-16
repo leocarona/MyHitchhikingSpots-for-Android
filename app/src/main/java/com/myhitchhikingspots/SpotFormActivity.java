748448c52796236e15a52c2e5266bdad2ea4331c
Leo Carona <leoncioleone@gmail.com>
2017-06-16 07:54:33 -0300

    Show panels even when network is not available (we can show the basic info that we already have instead)

--- app/src/main/java/com/myhitchhikingspots/SpotFormActivity.java
+++ app/src/main/java/com/myhitchhikingspots/SpotFormActivity.java
@@ -476,8 +476,8 @@ public class SpotFormActivity extends BaseActivity implements RatingBar.OnRating
 
         if (shouldRetrieveDetailsFromHW) {
             if (!Utils.isNetworkAvailable(this)) {
-                panel_buttons.setVisibility(View.GONE);
-                panel_info.setVisibility(View.GONE);
+                //panel_buttons.setVisibility(View.GONE);
+                //panel_info.setVisibility(View.GONE);
                 showErrorAlert("Offline mode", "Your device doesn't seem to have an internet connection at the moment? :-)");
             } else {
                 //we use getSnippet() for id because original hitchwiki id is stored as snippet in our markers
