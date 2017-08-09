5ab15904b63a9be32c3d70945108cd58bd67e6bf
Leo Carona <leoncioleone@gmail.com>
2017-08-09 20:10:35 -0300

    Adding Crashlytics to log errors on serverrequests

--- app/src/main/java/hitchwikiMapsSDK/classes/ServerRequest.java
+++ app/src/main/java/hitchwikiMapsSDK/classes/ServerRequest.java
@@ -1,5 +1,9 @@
 package hitchwikiMapsSDK.classes;
 
+import android.util.Log;
+
+import com.crashlytics.android.Crashlytics;
+
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -13,38 +17,42 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import hitchwikiMapsSDK.entities.Error;
 
-public class ServerRequest 
+public class ServerRequest
 {
 	String json = null;
 	InputStream is = null;
     JSONObject jObj;
     JSONArray jArray;
-    
+    static String TAG = "server-request";
+
     //hitchwiki
-    	
+
 	public JSONObject postRequest(String url)
 	{
+        // Making HTTP request
+        Crashlytics.log(Log.INFO, TAG, "Posting to url: " + url);
         String errorMsg = "";
 
-		// Making HTTP request
-		try 
+		try
 		{
             // defaultHttpClient
             DefaultHttpClient httpClient = new DefaultHttpClient();
             HttpPost httpPost = new HttpPost(url);
-            
+
             httpPost.addHeader("Content-Type", "application/x-www-form-urlencoded");
 //            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
             HttpResponse httpResponse = httpClient.execute(httpPost);
             HttpEntity httpEntity = httpResponse.getEntity();
             is = httpEntity.getContent();
-            System.out.println(is.toString());
-        } 
+            Crashlytics.log(Log.INFO, TAG, "Received result:" + is.toString());
+        }
 		catch (Exception e)
 		{
             errorMsg = e.getMessage();
+            Crashlytics.logException(e);
         }
 
+        //Read through the received result and convert into string (No idea why this is needed and got no time to analyse. This comes from an old code)
          if(errorMsg.isEmpty()) {
              try {
                  BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
@@ -56,9 +64,10 @@ public class ServerRequest
                  is.close();
                  sb.deleteCharAt(sb.length() - 1);
                  json = sb.toString();
-                 System.out.println("JSON after post: " + json);
+                 Crashlytics.log(Log.INFO, TAG, "Received result converted into string:" + json);
              } catch (Exception e) {
                  errorMsg = e.getMessage();
+                 Crashlytics.logException(e);
              }
          }
 
@@ -66,8 +75,9 @@ public class ServerRequest
             // try parse the string to a JSON object
             try {
                 jObj = new JSONObject(json);
-            } catch (JSONException e) {
+            } catch (Exception e) {
                 errorMsg = e.getMessage();
+                Crashlytics.logException(e);
             }
         }
 
@@ -76,9 +86,11 @@ public class ServerRequest
             // return JSON object
         return jObj;
 	}
-	
+
 	public String postRequestString(String url)
-	{// Making HTTP request
+	{
+        Crashlytics.log(Log.INFO, TAG, "Posting to url: " + url);
+        // Making HTTP request
 
         String errorMsg = "";
         try
@@ -86,18 +98,20 @@ public class ServerRequest
             // defaultHttpClient
             DefaultHttpClient httpClient = new DefaultHttpClient();
             HttpPost httpPost = new HttpPost(url);
-            
+
             httpPost.addHeader("Content-Type", "application/x-www-form-urlencoded");
             HttpResponse httpResponse = httpClient.execute(httpPost);
             HttpEntity httpEntity = httpResponse.getEntity();
             is = httpEntity.getContent();
-            System.out.println(is.toString());    
-        } 
+            Crashlytics.log(Log.INFO, TAG, "Received result:" + is.toString());
+        }
 		catch (Exception e)
 		{
             errorMsg = e.getMessage();
+            Crashlytics.logException(e);
         }
 
+        //Read through the received result and convert into string (No idea why this is needed and got no time to analyse. This comes from an old code)
         if(errorMsg.isEmpty()) {
             try {
                 BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
@@ -109,8 +123,10 @@ public class ServerRequest
                 is.close();
                 sb.deleteCharAt(sb.length() - 1);
                 json = sb.toString();
+                Crashlytics.log(Log.INFO, TAG, "Received result converted into string:" + json);
             } catch (Exception e) {
                 errorMsg = "Buffer Error: Error converting result.\n\"" + e.getMessage() + "\"";
+                Crashlytics.logException(e);
             }
         }
 
@@ -119,7 +135,7 @@ public class ServerRequest
         // return JSON string
         return json;
 	}
-	
+
 //	public JSONObject getRequest(String url, String token, String xApiKey)
 //	{
 //		try 
@@ -183,5 +199,5 @@ public class ServerRequest
 //        // return JSON String
 //        return jObj;
 //	}
-	
+
 }
