4dafddfa761c9247681d7531a434042c1fd54d8c
Leo Carona <leoncioleone@gmail.com>
2017-10-06 00:31:40 -0300

    Fix exception been thrown when trying to convert result into jSONObject in postRequestString
    Replacing DefaultHttpClient for HttpURLConnection, changing the way how
    we convert the requested url result into string, and fix an exception
    been thrown lately. The exception was because we began converting the
    requested url result into son on postRequestString when where shouldn’t
    be doing it.

--- app/src/main/java/hitchwikiMapsSDK/classes/ServerRequest.java
+++ app/src/main/java/hitchwikiMapsSDK/classes/ServerRequest.java
@@ -4,160 +4,92 @@ import android.util.Log;
 
 import com.crashlytics.android.Crashlytics;
 
-import java.io.BufferedReader;
+import java.io.BufferedInputStream;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import cz.msebera.android.httpclient.HttpEntity;
-import cz.msebera.android.httpclient.HttpResponse;
-import cz.msebera.android.httpclient.client.methods.HttpPost;
-import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import hitchwikiMapsSDK.entities.Error;
 
 public class ServerRequest {
-    String json = null;
+    String jsonResult = null;
     InputStream is = null;
     JSONObject jObj;
     JSONArray jArray;
     static String TAG = "server-request";
 
-    //hitchwiki
-
-    public JSONObject postRequest(String url) {
-        // Making HTTP request
-        Crashlytics.log(Log.INFO, TAG, "Posting to url: " + url);
-        json = "";
-        jObj = null;
+    public String postRequestString(String url) {
+        jsonResult = "";
         String errorMsg = "";
+        HttpURLConnection urlConnection;
 
         try {
-            // defaultHttpClient
-            DefaultHttpClient httpClient = new DefaultHttpClient();
-            HttpPost httpPost = new HttpPost(url);
+            // the url we wish to connect to
+            URL urlObj = new URL(url);
+            // open the connection to the specified URL
+            urlConnection = (HttpURLConnection) urlObj.openConnection();
+            // get the response from the server in an input stream
+            is = new BufferedInputStream(urlConnection.getInputStream());
 
-            httpPost.addHeader("Content-Type", "application/x-www-form-urlencoded");
-            HttpResponse httpResponse = httpClient.execute(httpPost);
-            HttpEntity httpEntity = httpResponse.getEntity();
-            is = httpEntity.getContent();
-            Crashlytics.log(Log.INFO, TAG, "Received result:" + is.toString());
+            // covert the input stream to a string
+            jsonResult = convertStreamToString(is);
+
         } catch (Exception e) {
             errorMsg = e.getMessage();
             Crashlytics.logException(e);
         }
 
-        //Read through the received result and convert into string (No idea why this is needed and got no time to analyse. This comes from an old code)
-        if (errorMsg.isEmpty()) {
-            try {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
-                StringBuilder sb = new StringBuilder();
-                String line = null;
-                while ((line = reader.readLine()) != null) {
-                    sb.append(line + "n");
-                }
-                is.close();
-                sb.deleteCharAt(sb.length() - 1);
-                json = sb.toString();
-                Crashlytics.log(Log.INFO, TAG, "Received result converted into string:" + json);
-            } catch (Exception e) {
-                errorMsg = e.getMessage();
-                Crashlytics.logException(e);
-            }
-        }
-
-        if (errorMsg.isEmpty()) {
-            // try parse the string to a JSON object
-            try {
-                jObj = new JSONObject(json);
-            } catch (Exception e) {
-                errorMsg = e.getMessage();
-                Crashlytics.logException(e);
-            }
-        }
+        Crashlytics.log(Log.INFO, TAG, "Received result converted into string:" + jsonResult);
 
         if (!errorMsg.isEmpty()) {
             Error er = new Error(true, errorMsg);
             jObj = er.toJSONObject();
-            json = er.toJSONString();
+            jsonResult = er.toJSONString();
+        }
+
+        // return JSON string
+        return jsonResult;
+    }
+
+    public JSONObject postRequest(String url) {
+        // Making HTTP request
+        Crashlytics.log(Log.INFO, TAG, "Posting to url: " + url);
+
+        //Set jObj to null here so that if postRequestString throws any exception, jOBj won't be null
+        jObj = null;
+
+        //Call postRequestString - the result to the call to url will be stored in the variable json
+        postRequestString(url);
+
+        //If an exception was thrown, jOBj will be an Error object converted to json string
+        if (jObj == null) {
+            String errorMsg = "";
+
+            // try parse the string to a JSON object
+            try {
+                jObj = new JSONObject(jsonResult);
+            } catch (Exception e) {
+                errorMsg = e.getMessage();
+                Crashlytics.logException(e);
+            }
+
+            if (!errorMsg.isEmpty()) {
+                Error er = new Error(true, errorMsg);
+                jObj = er.toJSONObject();
+                jsonResult = er.toJSONString();
+            }
         }
 
         // return JSON object
         return jObj;
     }
 
-    public String postRequestString(String url) {
-        postRequest(url);
-
-        // return JSON string
-        return json;
+    public String convertStreamToString(InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
     }
-
-//	public JSONObject getRequest(String url, String token, String xApiKey)
-//	{
-//		try 
-//		{
-//            // defaultHttpClient
-//            DefaultHttpClient httpClient = new DefaultHttpClient();
-//                                
-//            HttpGet httpGet = new HttpGet(url);
-//            
-//            httpGet.addHeader("X-API-KEY", xApiKey.toString());
-//            httpGet.addHeader("ACCESS-TOKEN", token.toString());
-//            
-//            HttpResponse httpResponse = httpClient.execute(httpGet);
-//            HttpEntity httpEntity = httpResponse.getEntity();
-//            is = httpEntity.getContent();
-//            System.out.println(is.toString());
-// 
-//        } 
-//		catch (UnsupportedEncodingException e) 
-//		{
-//           System.out.println("UnsupportedEncodingExceptio: UnsupportedEncodingExceptio");
-//        } 
-//		catch (ClientProtocolException e) 
-//		{
-//        	System.out.println("ClientProtocolException: ClientProtocolException");
-//        } 
-//		catch (IOException e) 
-//		{
-//        	System.out.println("IOException: IOException");
-//        }
-//        
-//		try 
-//		{
-//            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
-//            StringBuilder sb = new StringBuilder();
-//            String line = null;
-//            while ((line = reader.readLine()) != null) 
-//            {
-//                sb.append(line + "n");
-//            }
-//            is.close();
-//            sb.deleteCharAt(sb.length()-1);
-//            json = sb.toString();
-//            System.out.println("JSON after GET: "+ json); 
-//        } 
-//		catch (Exception e) 
-//		{
-//        	System.out.println("Buffer Error: Error converting result " + e.toString());
-//        }
-//        
-//		// try parse the string to a JSON object
-//        try 
-//        {
-//            jObj = new JSONObject(json);
-//        }
-//        catch (JSONException e) 
-//        {
-//            System.out.println("JSON Parser: Error parsing data " + e.toString());
-//        }
-// 
-//        // return JSON String
-//        return jObj;
-//	}
 
 }
