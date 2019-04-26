789b888d61c6414e509162d31a687e9fdcc0cd5d
Leo Carona <leoncioleone@gmail.com>
2019-04-26 20:59:25 -0300

    Zoom out of My Maps should display at least the most recent route #69

--- app/src/main/java/com/myhitchhikingspots/MyMapsFragment.java
+++ app/src/main/java/com/myhitchhikingspots/MyMapsFragment.java
@@ -762,8 +762,8 @@ public class MyMapsFragment extends Fragment implements OnMapReadyCallback, Perm
         }
 
         //Define the number of spots to be considered when zoomOutToFitMostRecentRoute is called.
-        //We want to consider at maximum the spots from the last route saved.
-        if (routes != null && routes.size() > 0 && routes.get(0).features.length < NUMBER_OF_SPOTS_TO_FIT)
+        //We want at least the spots from the most recent route to be included.
+        if (routes != null && routes.size() > 0 && routes.get(0).features.length > NUMBER_OF_SPOTS_TO_FIT)
             NUMBER_OF_SPOTS_TO_FIT = routes.get(0).features.length;
 
         PolylineOptions[] allPolylines = new PolylineOptions[routes.size()];
