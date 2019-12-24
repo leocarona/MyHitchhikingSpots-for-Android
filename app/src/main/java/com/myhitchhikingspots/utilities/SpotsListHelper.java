938513e2a2e8657da855c55ef4ec6da43a93cabe
Leo Carona <leoncioleone@gmail.com>
2019-12-24 17:28:56 -0200

    Adding more stats to Dashboard
    Adding 'Your shortest waiting times have been between' and 'You're usually picked up between' stats.

--- app/src/main/java/com/myhitchhikingspots/utilities/SpotsListHelper.java
+++ app/src/main/java/com/myhitchhikingspots/utilities/SpotsListHelper.java
@@ -1,8 +1,11 @@
 package com.myhitchhikingspots.utilities;
 
+import androidx.core.util.Pair;
+
 import com.mapbox.geojson.Feature;
 import com.myhitchhikingspots.Constants;
 import com.myhitchhikingspots.MyMapsFragment;
+import com.myhitchhikingspots.model.Spot;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
@@ -11,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
+import java.util.TimeZone;
 
 public class SpotsListHelper {
 
@@ -153,5 +157,169 @@ public class SpotsListHelper {
     public static boolean shouldIncludeSpot(Calendar calDate, Calendar calStartsOn, Calendar calEndsOn) {
         return calDate.equals(calStartsOn) || calDate.equals(calEndsOn) ||
                 (calDate.after(calStartsOn) && calDate.before(calEndsOn));
+    }
+
+    public static List<Pair<Integer, Integer>> getNumberOfOccurrences(List<Spot> spotList) {
+        if (spotList.isEmpty())
+            return new ArrayList<>();
+        int[] numberOfOccurrencesPerHour = getNumberOfOccurrencesPerHour(spotList);
+
+        List<Pair<Integer, Integer>> result = new ArrayList<>();
+        //Add a position to each hour of the day (0-23h) and how many rides were hitched at each hour
+        for (int hour = 0; hour < numberOfOccurrencesPerHour.length; hour++)
+            result.add(new Pair<>(hour, numberOfOccurrencesPerHour[hour]));
+
+        // Sort from the hour where more rides were gotten to the hour where less rides were gotten.
+        Collections.sort(result, (integerIntegerPair, t1) -> {
+            if (integerIntegerPair.second < t1.second)
+                return 1;
+            else if (integerIntegerPair.second > t1.second)
+                return -1;
+            else
+                return 0;
+        });
+
+        return result;
+    }
+
+    public static int[] getNumberOfOccurrencesPerHour(List<Spot> spots) {
+        int[] result = new int[24];
+        for (Spot spot : spots) {
+            if (spot.getAttemptResult() != null && spot.getAttemptResult() == Constants.ATTEMPT_RESULT_GOT_A_RIDE && spot.getWaitingTime() != null) {
+                Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+                cal.setTime(spot.getStartDateTime().toDate());
+                cal.add(Calendar.MINUTE, spot.getWaitingTime());
+                int hourOfOccurrence = cal.get(Calendar.HOUR_OF_DAY);
+                result[hourOfOccurrence]++;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Get how many rides were hitched at each interval of 30 min.
+     *
+     * @return A list of pair where
+     * the first element is another pair informing the period of time (0-30min; 30-60min; 60-90min; etc) and
+     * the second element is the number of spots hitched within that interval.
+     **/
+    public static List<Pair<Pair<Integer, Integer>, Integer>> getWaitingTimeOccurrences(List<Spot> spotList) {
+        if (spotList.isEmpty())
+            return new ArrayList<>();
+        List<Integer> numberOfOccurrencesPerHalfHour = getWaitingTimeOccurrencesEachHalfHour(spotList);
+
+        List<Pair<Pair<Integer, Integer>, Integer>> result = new ArrayList<>();
+        //Add a position to each interval of half hour (0-30; 30-60; 60-90; etc) and how many rides were hitched at interval
+        for (int coefficient = 1; coefficient <= numberOfOccurrencesPerHalfHour.size(); coefficient++) {
+            Pair<Integer, Integer> periodOfTime = new Pair<>((coefficient - 1) * 30, coefficient * 30);
+            result.add(new Pair<>(periodOfTime, numberOfOccurrencesPerHalfHour.get(coefficient - 1)));
+        }
+
+        // Sort from the period of time where more rides were gotten to the period of time where less rides were gotten.
+        Collections.sort(result, (integerIntegerPair, t1) -> {
+            if (integerIntegerPair.second < t1.second)
+                return 1;
+            else if (integerIntegerPair.second > t1.second)
+                return -1;
+            else
+                return 0;
+        });
+
+        return result;
+    }
+
+    /**
+     * Get how many times user got rides within periods of half hour.
+     * The index on the list is used to calculate the period of time, and the number on the list represents the number of rides gotten.
+     *
+     * @return List of number of occurrences within the period of time.
+     * The index on the list plus 1 or 2 respectively represent the coefficient to be multiplied by 30 in order to find what's the beginning and the ending of the period of time.
+     * E.g: if you find the number 25 on the index 2 of the list, you know that between 60-90min. How?
+     * Do the math as follows:
+     * (2[indexOnTheList] + 1) = 2[coefficient to find the beginning of the period];
+     * (2[indexOnTheList] + 2) = 3[coefficient to find the ending of the period];
+     * 2[coefficient to find the beginning of the period]*30 = 60;
+     * 3[coefficient to find the beginning of the period]*30 = 90.
+     * Because 25 is at the index 2 on the list you interpret that between 60-90min of waiting the user has gotten 25 rides.
+     **/
+    public static List<Integer> getWaitingTimeOccurrencesEachHalfHour(List<Spot> spots) {
+        int numOfSpotsToCheck = getNumberOfRidesGotten(spots);
+        List<Integer> result = new ArrayList<>();
+        int spotsAccounted = 0;
+        int times = 1;
+        int min, max;
+        // For each interval of 30 min, check how many Got A Ride spots have their waiting times within such interval
+        while (spotsAccounted < numOfSpotsToCheck) {
+            min = 30 * (times - 1);
+            max = 30 * times;
+            int occurancesWithinPeriodOfTime = 0;
+            for (Spot spot : spots) {
+                if (spot.getAttemptResult() != null && spot.getAttemptResult() == Constants.ATTEMPT_RESULT_GOT_A_RIDE &&
+                        spot.getWaitingTime() != null && spot.getWaitingTime() >= min && spot.getWaitingTime() < max) {
+                    occurancesWithinPeriodOfTime++;
+                    spotsAccounted++;
+
+                    //Nothing else to check, stop for loop
+                    if (spotsAccounted == numOfSpotsToCheck)
+                        break;
+                }
+            }
+            result.add(occurancesWithinPeriodOfTime);
+            times++;
+        }
+        return result;
+    }
+
+    public static int getNumberOfRidesGotten(List<Spot> spotList) {
+        int numOfRides = 0;
+        for (Spot spot : spotList)
+            if (spot.getAttemptResult() != null && spot.getAttemptResult() == Constants.ATTEMPT_RESULT_GOT_A_RIDE)
+                numOfRides++;
+        return numOfRides;
+    }
+
+    public static int getShortestWaitingTime(List<Spot> spotList){
+        Integer  shortestWaitingTime = 0;
+
+        if (spotList.size() > 0)
+            shortestWaitingTime = spotList.get(0).getWaitingTime() == null ? 0 : spotList.get(0).getWaitingTime();
+
+        for (Spot spot : spotList) {
+            Boolean isDestination = spot.getIsDestination() == null ? false : spot.getIsDestination();
+            Boolean isHitchhikingSpot = spot.getIsHitchhikingSpot() == null ? false : spot.getIsHitchhikingSpot();
+
+            if (isHitchhikingSpot) {
+
+                if (!isDestination) {
+                    Integer waitingTime = spot.getWaitingTime() == null ? 0 : spot.getWaitingTime();
+                    //Only consider spots where the user has gotten rides
+                    if (spot.getAttemptResult() != null && spot.getAttemptResult() == Constants.ATTEMPT_RESULT_GOT_A_RIDE) {
+                        if (waitingTime < shortestWaitingTime)
+                            shortestWaitingTime = waitingTime;
+                    }
+                }
+            }
+        }
+        return shortestWaitingTime;
+    }
+
+    public static int getLongestWaitingTime(List<Spot> spotList){
+        int longestWaitingTime=0;
+        if (spotList.size() > 0)
+            longestWaitingTime = spotList.get(0).getWaitingTime() == null ? 0 : spotList.get(0).getWaitingTime();;
+
+        for (Spot spot : spotList) {
+            Boolean isDestination = spot.getIsDestination() == null ? false : spot.getIsDestination();
+            Boolean isHitchhikingSpot = spot.getIsHitchhikingSpot() == null ? false : spot.getIsHitchhikingSpot();
+
+            if (isHitchhikingSpot) {
+                if (!isDestination) {
+                    Integer waitingTime = spot.getWaitingTime() == null ? 0 : spot.getWaitingTime();
+                    if (waitingTime > longestWaitingTime)
+                        longestWaitingTime = waitingTime;
+                }
+            }
+        }
+        return longestWaitingTime;
     }
 }
