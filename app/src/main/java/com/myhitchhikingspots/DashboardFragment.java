d0349fce318c7b5a3b19f95755b7fffecbb2bb0d
Leo Carona <leoncioleone@gmail.com>
2020-07-22 13:34:28 -0300

    fixing bug that was preventing a second+ buton click

--- app/src/main/java/com/myhitchhikingspots/DashboardFragment.java
+++ app/src/main/java/com/myhitchhikingspots/DashboardFragment.java
@@ -244,4 +244,10 @@ public class DashboardFragment extends Fragment {
         } catch (Exception e) {
         }
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        isHandlingRequestToOpenSpotForm = false;
+    }
 }
