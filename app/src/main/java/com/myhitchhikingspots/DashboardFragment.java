e2f087c45b5d0e42fd048707e2b77563a8722fea
Leo Carona <leoncioleone@gmail.com>
2020-07-24 15:41:55 -0300

    adding missing commit

--- app/src/main/java/com/myhitchhikingspots/DashboardFragment.java
+++ app/src/main/java/com/myhitchhikingspots/DashboardFragment.java
@@ -14,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.util.Pair;
 import androidx.fragment.app.Fragment;
